51f3005b67b837f279b5e66f256c6d7135e0a7b7
ltpitt <d.nastri@gmail.com>
2018-03-25 16:50:50 +0200

    feat: Added DifferentSorters class

--- src/DifferentSorters.java
+++ src/DifferentSorters.java
@@ -9,15 +9,26 @@
 import java.util.*;
 
 public class DifferentSorters {
+
+
+    //String source = "src\\data\\nov20quakedatasmall.atom";
+    String source = "src\\data\\nov20quakedata.atom";
+    //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+
+
+    public DifferentSorters() {
+    }
+
     public void sortWithCompareTo() {
         EarthQuakeParser parser = new EarthQuakeParser();
-        String source = "data/nov20quakedata.atom";
-        //String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         Collections.sort(list);
         for(QuakeEntry qe: list) {
             System.out.println(qe);
         }
+        int quakeNumber = 10;
+        System.out.println("Print quake entry in position " + quakeNumber);
+        System.out.println(list.get(quakeNumber));
 
     }    
 
@@ -46,4 +57,16 @@ public class DifferentSorters {
         }
 
     }
+
+    public static void main(String[] args) {
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
+        DifferentSorters ds = new DifferentSorters();
+        ds.sortWithCompareTo();
+        //EarthQuakeClient2 client = new EarthQuakeClient2();
+        //client.quakesWithFilter();
+        //client.testMatchAllFilter();
+        //client.testMatchAllFilter2();
+    }
+
 }
